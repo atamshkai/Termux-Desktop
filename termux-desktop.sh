7e470d248d041d92f1b0478507ef16432b55150d
atamshkai <66311425+atamshkai@users.noreply.github.com>
2024-08-17 21:58:49 +0630

    termux-desktop.sh

--- termux-desktop.sh
+++ termux-desktop.sh
@@ -1,4 +1,4 @@
-pkg up -y && pkg i -y x11-repo tur-repo && pkg i -y audacious parole zsh proot-distro pulseaudio termux-x11-nightly chromium firefox xfce4 xfce4-goodies feathernotes xfce-theme-manager xfce4-appmenu-plugin geany
+pkg up -y && pkg i -y x11-repo tur-repo && pkg i -y audacious parole zsh proot-distro pulseaudio termux-x11-nightly chromium firefox xfce4 xfce4-goodies feathernotes xfce-theme-manager xfce4-terminal xfce4-appmenu-plugin geany
 cd $HOME
 wget https://github.com/atamshkai/Termux-Zsh/raw/main/zsh.tar.xz 
 tar -xvJf zsh.tar.xz && mv ~/zsh/.* ~/
@@ -7,7 +7,7 @@ chsh -s zsh
 echo "killall pulseaudio &>/dev/null" >>~/.zshrc 
 echo "pulseaudio --start --exit-idle-time=-1; pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" >>~/.zshrc 
 rm -rf ~/.config 
-wget https://archive.org/download/atam-shkai-termux-desktop/termux-desktop.tar.xz
+wget https://archive.org/download/atamshkai/termux-desktop.tar.xz
 tar -xvJf termux-desktop.tar.xz
 echo "termux-x11 :0 -xstartup 'dbus-launch --exit-with-session startxfce4' &>/dev/null" >>~/../usr/bin/tm-x11
 chmod +x ~/../usr/bin/tm-x11

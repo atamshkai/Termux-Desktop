1b7f19ddeaeb49b8e3f29189222dc0f4747723ab
atamshkai <66311425+atamshkai@users.noreply.github.com>
2024-09-10 16:41:39 +0630

    termux-desktop.sh

--- termux-desktop.sh
+++ termux-desktop.sh
@@ -1,6 +1,10 @@
-pkg up -y && pkg i -y x11-repo tur-repo && pkg i -y wget audacious parole zsh proot-distro pulseaudio termux-x11-nightly chromium firefox xfce4 xfce4-goodies feathernotes xfce-theme-manager xfce4-terminal xfce4-appmenu-plugin geany
+pkg up -y && pkg i -y x11-repo tur-repo && pkg i -y wget audacious parole zsh proot-distro pulseaudio termux-x11-nightly chromium firefox xfce4 xfce4-goodies feathernotes xfce-theme-manager xfce4-terminal xfce4-appmenu-plugin geany tmux pulseaudio-glib tmux alsa-utils ncpamixer
 cd $HOME
-rm -rf ~/.zshrc
+wget https://mirrors.sdu.edu.cn/termux/termux-x11/pool/main/p/pavucontrol-qt/pavucontrol-qt_2.0.0_aarch64.deb
+chmod +x pavucontrol-qt_2.0.0_aarch64.deb
+dpkg -i pavucontrol-qt_2.0.0_aarch64.deb
+apt-get --fix-broken install
+mv ~/.zshrc ~/.zshrc.old
 rm -rf ~/termux-desktop.tar.xz
 wget https://github.com/atamshkai/Termux-Zsh/raw/main/zsh.tar.xz 
 tar -xvJf zsh.tar.xz
